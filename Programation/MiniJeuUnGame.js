ac77dba5f32f9bb8c1de31d107719f2f8661f8ea
Ugo Tbn <59653406+UgoTabn@users.noreply.github.com>
2020-06-14 09:32:32 +0200

    Update MiniJeuUnGame.js

--- Programation/MiniJeuUnGame.js
+++ Programation/MiniJeuUnGame.js
@@ -7,12 +7,13 @@ class MiniJeuUnGame extends Phaser.Scene {
 preload(){
     this.load.image('eye', 'assets/pics/lance-overdose-loader-eye.png');
 	this.load.image('route', 'assets/map.png');
+	this.load.image('arrivee', 'assets/map.png');
+	
 }
 
 create(){
-	let miniJeu_Mort;//reinitialisation
 	//drag and drop de la voiture
-	let voiture = this.physics.add.sprite(100, 100, 'eye').setInteractive();;
+	voiture = this.physics.add.sprite(100, 100, 'eye').setInteractive();
 	this.input.setDraggable(voiture);
 	this.input.dragDistanceThreshold = 16;
 	this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
@@ -21,20 +22,25 @@ create(){
 	});
 	
 	let route = this.physics.add.staticGroup();
+	let arrivee = this.physics.add.staticGroup();
 	route.create(300, 300, 'route').setOrigin(0).refreshBody();
+	arrivee.create(200, 200, 'arrivee').setOrigin(0).refreshBody();
 	this.physics.add.collider(voiture, route, touche, null, this);
+	this.physics.add.collider(voiture, arrivee, finNiveau, null, this);
 	
-	let timer = this.time.addevent({delay : 5000, callback: functionTimer, loop : false});
+this.time.addEvent({ delay: 5000, callback:chrono, callbackScope: this, loop: false });
 
 
 }
 
 	
 update(){
- if(miniJeu_Mort ===1){
-	 this.scene.start("MiniJeuUnUniquement");
- }
-
+if(miniJeu_Mort===1 && modeEntrainement ===1){
+	this.scene.start("MiniJeuUnUniquement");
+}
+if(miniJeu_Mort===1 && modeEntrainement ===0){
+	this.scene.start("PartieNormale");
+}
 
 
 }
