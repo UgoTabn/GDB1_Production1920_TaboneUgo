e47c2673c871019e62f709d3f175da7d8375c176
Ugo Tbn <59653406+UgoTabn@users.noreply.github.com>
2020-06-14 09:32:29 +0200

    Update Menu.js

--- Programation/Menu.js
+++ Programation/Menu.js
@@ -9,6 +9,13 @@ preload(){
 }
 
 create(){
+	//reset des valeurs
+	pointDeVie = 4;
+	miniJeu_Mort = 0;
+	miniJeu_Reussi = -1;
+	score = 0;
+	modeEntrainement = 0;
+
 	//boutons qui servent à se ballader dans les différents menus, chaque menu etant une scene
 	this.button = this.add.sprite(10, 10, 'jouer').setInteractive();
 	this.button.on('pointerdown', function(){this.scene.start("MenuJouer");}, this) //bouton pour aller dans le menu "Jouer"
