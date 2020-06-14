49e3eeff7e4b6953276da2027b73f0f44889198b
Ugo Tbn <59653406+UgoTabn@users.noreply.github.com>
2020-06-14 05:27:01 +0200

    Create MenuChoixMiniJeu.js

---
+++ Programation/MenuChoixMiniJeu.js
@@ -0,0 +1,28 @@
+class MenuChoixMiniJeu extends Phaser.Scene {
+  constructor(){
+    super("MenuChoixMiniJeu")
+  }
+
+	
+preload(){
+
+}
+
+create(){
+	this.button = this.add.sprite(10, 10, 'retour').setInteractive();
+	this.button.on('pointerdown', function(){this.scene.start("Menu");}, this) //bouton pour aller dans le menu "Menu"
+	
+	this.button = this.add.sprite(50, 10, 'jeuUn').setInteractive();
+	this.button.on('pointerdown', function(){this.scene.start("MiniJeuUnUniquement");}, this) //bouton pour lancer le jeu un
+	
+	this.button = this.add.sprite(100, 10, 'jeuDeux').setInteractive();
+	this.button.on('pointerdown', function(){this.scene.start("MiniJeuDeuxUniquement");}, this) //bouton pour lancer le jeu deux
+	
+	this.button = this.add.sprite(150, 10, 'jeuTrois').setInteractive();
+	this.button.on('pointerdown', function(){this.scene.start("MiniJeuTroisUniquement");}, this) //bouton pour lancer le jeu trois
+}
+	
+update(){
+ 
+}
+}
