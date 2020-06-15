3bef5c51b17c0573ce84d37f32e58e8a7e76fa4b
Ugo Tbn <59653406+UgoTabn@users.noreply.github.com>
2020-06-15 06:02:44 +0200

    Update MiniJeuDeuxGame.js

--- Programation/MiniJeuDeuxGame.js
+++ Programation/MiniJeuDeuxGame.js
@@ -5,23 +5,23 @@ class MiniJeuDeuxGame extends Phaser.Scene {
 
 	
 preload(){
-    this.load.image('eye', 'assets/pics/lance-overdose-loader-eye.png');
-	this.load.image('route', 'assets/map.png');
+    this.load.image('roue', 'assets/pics/lance-overdose-loader-eye.png');
+	this.load.image('boulon', 'assets/map.png');
+	this.load.image('fond', 'assets/map.png');
 }
 
 create(){
 	//drag and drop de la voiture
-	let voiture = this.physics.add.sprite(100, 100, 'eye').setInteractive();;
-	this.input.setDraggable(voiture);
+	let roue = this.physics.add.sprite(100, 100, 'roue').setInteractive();
+	this.input.setDraggable(roue);
+	
 	this.input.dragDistanceThreshold = 16;
 	this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
 		gameObject.x = dragX;
         gameObject.y = dragY;
 	});
 	
-	let route = this.physics.add.staticGroup();
-	route.create(300, 300, 'route').setOrigin(0).refreshBody();
-	this.physics.add.collider(voiture, route, touche, null, this);
+
 
 
 
