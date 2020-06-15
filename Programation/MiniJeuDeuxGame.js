642c96e614ca480937534914f54f7c2fed57544c
Ugo Tbn <59653406+UgoTabn@users.noreply.github.com>
2020-06-15 13:20:47 +0200

    Update MiniJeuDeuxGame.js

--- Programation/MiniJeuDeuxGame.js
+++ Programation/MiniJeuDeuxGame.js
@@ -13,7 +13,7 @@ preload(){
 create(){
 	//drag and drop de la voiture
 	let roue = this.physics.add.sprite(100, 100, 'roue').setInteractive();
-	boulon = 0;
+	
 	this.input.setDraggable(roue);
 	this.input.dragDistanceThreshold = 16;
 
@@ -21,7 +21,12 @@ create(){
 	boulonun = this.button = this.add.sprite(10, 10, 'boulon').setInteractive();
 	boulonun.on('pointerdown', function(){boulon = 1;}, this) //bouton pour aller dans le menu "Menu"
 
+	let arrivee = this.physics.add.staticGroup();
+	arrivee.create(200, 600, 'arrivee').setOrigin(0).refreshBody();
+	this.physics.add.collider(roue, arrivee, finNiveauDeux, null, this);
 
+	
+	this.time.addEvent({ delay: 5000, callback:chrono, callbackScope: this, loop: false });
 
 
 }
@@ -39,6 +44,13 @@ update(){
 	if(boulon === 1){
 		boulonun.destroy();
 	}
+	
+	if(miniJeu_Mort===1 && modeEntrainement ===1){
+	this.scene.start("MiniJeuDeuxUniquement");
+	}
+	if(miniJeu_Mort===1 && modeEntrainement ===0){
+	this.scene.start("PartieNormale");
+}
 
 }
 }
