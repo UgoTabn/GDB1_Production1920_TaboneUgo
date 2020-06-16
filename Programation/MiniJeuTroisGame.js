7d53a57734ccb56b55c82037e4a0a366aa0981eb
Ugo Tbn <59653406+UgoTabn@users.noreply.github.com>
2020-06-16 12:47:44 +0200

    Update MiniJeuTroisGame.js

--- Programation/MiniJeuTroisGame.js
+++ Programation/MiniJeuTroisGame.js
@@ -5,31 +5,52 @@ class MiniJeuTroisGame extends Phaser.Scene {
 
 	
 preload(){
-    this.load.image('eye', 'assets/pics/lance-overdose-loader-eye.png');
-	this.load.image('route', 'assets/map.png');
+    this.load.image('pilote', 'assets/pics/lance-overdose-loader-eye.png');
+	this.load.image('habit1', 'assets/map.png');
+	this.load.image('habit2', 'assets/map.png');
+	this.load.image('habit3', 'assets/map.png');
+	this.load.image('habit4', 'assets/map.png');
 }
 
 create(){
-	//drag and drop de la voiture
-	let voiture = this.physics.add.sprite(100, 100, 'eye').setInteractive();;
-	this.input.setDraggable(voiture);
+	let habit1 = this.physics.add.sprite(50, 100, 'habit1').setInteractive();
+	let habit2 = this.physics.add.sprite(50, 150, 'habit2').setInteractive();
+	let habit3 = this.physics.add.sprite(50, 200, 'habit3').setInteractive();
+	let habit4 = this.physics.add.sprite(50, 250, 'habit4').setInteractive();
+	
+	nombreHabitRestant = 2;
+	
+	this.input.setDraggable(habit1);
+	this.input.setDraggable(habit2);
+	this.input.setDraggable(habit3);
+	this.input.setDraggable(habit4);
 	this.input.dragDistanceThreshold = 16;
+	
 	this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
 		gameObject.x = dragX;
         gameObject.y = dragY;
 	});
 	
-	let route = this.physics.add.staticGroup();
-	route.create(300, 300, 'route').setOrigin(0).refreshBody();
-	this.physics.add.collider(voiture, route, touche, null, this);
+	let pilote = this.physics.add.staticGroup();
+	pilote.create(200, 200, 'pilote');
+	this.physics.add.collider(habit1, pilote,toucheObjetTypeUn, null, this);
+	this.physics.add.collider(habit2, pilote,toucheObjetTypeUn, null, this);
+	this.physics.add.collider(habit3, pilote,toucheObjetTypeDeux, null, this);
+	this.physics.add.collider(habit4, pilote,toucheObjetTypeDeux, null, this);
+	
 
-
-
+	
+	this.time.addEvent({ delay: 5000, callback:chrono, callbackScope: this, loop: false });
 }
 
 	
 update(){
- 	
+ 	if(miniJeu_Mort===1 && modeEntrainement ===1){
+	this.scene.start("MiniJeuTroisUniquement");
+}
+if(miniJeu_Mort===1 && modeEntrainement ===0){
+	this.scene.start("PartieNormale");
+}
 
 
 
