8c5d7e770c7045b1ec5530cf3cf93dda9743488b
Ugo Tbn <59653406+UgoTabn@users.noreply.github.com>
2020-06-16 12:47:37 +0200

    Update DriverClash.js

--- Programation/DriverClash.js
+++ Programation/DriverClash.js
@@ -18,6 +18,7 @@ let miniJeu_Mort;
 let miniJeu_RecordUn;
 let miniJeu_RecordDeux;
 let miniJeu_RecordTrois;
+let jeu_Record;
 let pointDeVie;
 let DebutJeuUn;
 let voiture;
@@ -27,6 +28,9 @@ let dialogue;
 let style;
 let barreDeVie;
 let modeEntrainement;
+let boulon;
+let boulonun;
+let nombreHabitRestant;
 
 function touche(route, voiture){
 	miniJeu_Mort = 1;
@@ -36,7 +40,22 @@ function touche(route, voiture){
 function finNiveau(arrivee, voiture){
 	score +=1;
 	miniJeu_Reussi = 1;
+	if(modeEntrainement===1){
 	this.scene.start("MiniJeuUnUniquement");
+	}
+	else if(modeEntrainement===0){
+	this.scene.start("PartieNormale");
+	}
+};
+function finNiveauDeux(arrivee, roue){
+	score +=1;
+	miniJeu_Reussi = 1;
+	if(modeEntrainement===1){
+	this.scene.start("MiniJeuDeuxUniquement");
+	}
+	else if(modeEntrainement===0){
+	this.scene.start("PartieNormale");
+	}
 };
 
 function chrono(){
@@ -44,8 +63,35 @@ function chrono(){
 	pointDeVie -=1;
 };
 function enchainementDeScene(){
+	if(modeEntrainement===1){
 	if(pointDeVie>0){this.scene.start("MiniJeuUnGame");}
-	else if(pointDeVie===0){this.scene.start("Menu");miniJeu_RecordUn = score;}
+	else if(pointDeVie===0){if(miniJeu_RecordUn<score){miniJeu_RecordUn = score;}this.scene.start("Menu");}
+	}
+	else if(modeEntrainement===0){
+	if(pointDeVie>0){this.scene.start("PartieNormale");}
+	else if(pointDeVie===0){if(jeu_Record<score){jeu_Record = score;}this.scene.start("Menu");}
+	}
+};
+function enchainementDeSceneDeux(){
+	if(modeEntrainement===1){
+	if(pointDeVie>0){this.scene.start("MiniJeuDeuxGame");}
+	else if(pointDeVie===0){if(miniJeu_RecordDeux<score){miniJeu_RecordDeux = score;}this.scene.start("Menu");}
+	}
+	else if(modeEntrainement===0){
+	if(pointDeVie>0){this.scene.start("PartieNormale");}
+	else if(pointDeVie===0){if(jeu_Record<score){jeu_Record = score;}this.scene.start("Menu");}
+	}
+};
+function enchainementDeSceneTrois(){
+	if(modeEntrainement===1){
+	if(pointDeVie>0){this.scene.start("MiniJeuTroisGame");}
+	else if(pointDeVie===0){if(miniJeu_RecordTrois<score){miniJeu_RecordTrois = score;}this.scene.start("Menu");}
+	}
+	else if(modeEntrainement===0){
+	if(pointDeVie>0){this.scene.start("PartieNormale");}
+	else if(pointDeVie===0){if(jeu_Record<score){jeu_Record = score;}this.scene.start("Menu");}
+	}
+
 };
 function majHP(){
 			if(pointDeVie===3){
@@ -55,11 +101,27 @@ function majHP(){
 	   barreDeVie.anims.play('depv',true);
 	   }
 	 		if(pointDeVie===1){
-	   barreDeVie.anims.play('1trpv',true);
+	   barreDeVie.anims.play('trpv',true);
 	   }
 };
-
-
+function toucheObjetTypeUn(habit, pilote){
+	habit.destroy();
+	nombreHabitRestant-=1;
+	if(nombreHabitRestant===0){
+	score +=1;
+	miniJeu_Reussi = 1;
+	if(modeEntrainement===1){
+	this.scene.start("MiniJeuTroisUniquement");
+	}
+	else if(modeEntrainement===0){
+	this.scene.start("PartieNormale");
+	}
+}
+};
+function toucheObjetTypeDeux(habit, pilote){
+	miniJeu_Mort = 1;
+	pointDeVie -=1;
+};
 
 
 
