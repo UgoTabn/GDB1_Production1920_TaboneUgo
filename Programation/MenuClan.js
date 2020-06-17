91ce97f62f1b73549e0e9e5b37749482530dce58
Ugo Tbn <59653406+UgoTabn@users.noreply.github.com>
2020-06-17 06:33:40 +0200

    Update MenuClan.js

--- Programation/MenuClan.js
+++ Programation/MenuClan.js
@@ -5,11 +5,13 @@ class MenuClan extends Phaser.Scene {
 
 	
 preload(){
-
+	this.load.image('backgroundclan', 'assets/backgroundclan.png'); //Background du menu avec les boutons
+	this.load.image('retour', 'assets/boutonretour.png'); //Background du menu avec les boutons
 }
 
 create(){
-	this.button = this.add.sprite(10, 10, 'retour').setInteractive();
+	this.add.image(0,0, 'backgroundclan').setOrigin(0);
+	this.button = this.add.sprite(55.8, 46.9, 'retour').setInteractive();
 	this.button.on('pointerdown', function(){this.scene.start("Menu");}, this) //bouton pour aller dans le menu "Menu"
 	
 	//indication sur le clan
